7e848c11d25fb6841cc3a53d9dce3b2f49e21a71
emacatriquil <56092756+emanuelctq@users.noreply.github.com>
2019-10-16 02:15:41 -0300

    Actualiza el archivo
    Se eliminan comentarios innecesarios y se agregan otros para mayor legibilidad. También se corrije la forma de mostrar los datos al usuario para poder utilizar el textarea

--- tareas/clase-5/tarea2.js
+++ tareas/clase-5/tarea2.js
@@ -5,30 +5,18 @@
 // Y va a cambiar el <h1> para decir "Bienvenido, nombreDeUsuario"!
 
 document.querySelector('#ingresar').onclick = () => {
+  // Obtener datos del usuario
   const primerNombreUsuario = document.querySelector('#primer-nombre-usuario').value;
   const segundoNombreUsuario = document.querySelector('#segundo-nombre-usuario').value;
   const apellidoUsuario = document.querySelector('#apellido-usuario').value;
   const edadUsuario = Number(document.querySelector('#edad-usuario').value);
+
+  // Personalizar saludo
   const titulo = document.querySelector('#titulo-principal');
-  const campoDeTexto = document.querySelector('#resultado');
   titulo.textContent = `Bienvenido/a ${primerNombreUsuario}`;
-  const contenidoResultado = document.createTextNode(`Tu nombre es: ${primerNombreUsuario} ${segundoNombreUsuario} ${apellidoUsuario}. Tenes ${edadUsuario} años`);
-  campoDeTexto.appendChild(contenidoResultado);
+
+  // Mostrar los datos en un text area
+  const contenidoResultado = `Tu nombre es: ${primerNombreUsuario} ${segundoNombreUsuario} ${apellidoUsuario}. Tenes ${edadUsuario} años`;
+  document.querySelector('#resultado').value = contenidoResultado;
 
 }
-// const nodoPagina = document.querySelector('body');
-// const nuevoParrafo = document.createElement('p');
-// const textoParrafo = document.createTextNode('Yeeee!');
-// nuevoParrafo.appendChild(textoParrafo);
-// nodoPagina.appendChild(nuevoParrafo);
-
-/*
-Ejemplo form:
-  <form id="entrada-bar" onsubmit="return false;">
-    <input type="text" placeholder="Ingresá tu nombre" id="nombre-usuario"/>
-    <input type="number" placeholder="Ingresá tu edad" id="edad-usuario" />
-    <input type="submit" value="Ingresar" id="ingresar"/>
-  </form>
-
-  <div id="resultado">Hola</div>
-*/

87ef3091359014e6d605ebd0a14beb3a6a33a8e2
emacatriquil <56092756+emanuelctq@users.noreply.github.com>
2019-10-15 22:41:20 -0300

    Maneja datos ingresados por el usuario
    Para poder personalizar el saludo de bienvenida con el nombre del usuario y mostrar sus datos en conjunto.

--- tareas/clase-5/tarea2.js
+++ tareas/clase-5/tarea2.js
@@ -4,6 +4,24 @@
 // mostrar toda la información junta en un campo de texto
 // Y va a cambiar el <h1> para decir "Bienvenido, nombreDeUsuario"!
 
+document.querySelector('#ingresar').onclick = () => {
+  const primerNombreUsuario = document.querySelector('#primer-nombre-usuario').value;
+  const segundoNombreUsuario = document.querySelector('#segundo-nombre-usuario').value;
+  const apellidoUsuario = document.querySelector('#apellido-usuario').value;
+  const edadUsuario = Number(document.querySelector('#edad-usuario').value);
+  const titulo = document.querySelector('#titulo-principal');
+  const campoDeTexto = document.querySelector('#resultado');
+  titulo.textContent = `Bienvenido/a ${primerNombreUsuario}`;
+  const contenidoResultado = document.createTextNode(`Tu nombre es: ${primerNombreUsuario} ${segundoNombreUsuario} ${apellidoUsuario}. Tenes ${edadUsuario} años`);
+  campoDeTexto.appendChild(contenidoResultado);
+
+}
+// const nodoPagina = document.querySelector('body');
+// const nuevoParrafo = document.createElement('p');
+// const textoParrafo = document.createTextNode('Yeeee!');
+// nuevoParrafo.appendChild(textoParrafo);
+// nodoPagina.appendChild(nuevoParrafo);
+
 /*
 Ejemplo form:
   <form id="entrada-bar" onsubmit="return false;">

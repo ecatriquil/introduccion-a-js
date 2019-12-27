9996134d10c17ad1cd1bcb8adbfa25fbf9a2d527
Emanuel Catriquil <56092756+ecatriquil@users.noreply.github.com>
2019-12-27 13:32:34 -0300

    Create desafio-3.js

---
+++ tareas/clase-4/desafios/desafio-3.js
@@ -0,0 +1,8 @@
+// Desafío de programación #3: Imprimí la tabla de multiplicación del 7
+function tablaDelSiete() {
+   for (let i = 0; i <= 10; i++) {
+      console.log(`7 x ${i} = ${i * 7}`);
+   }
+}
+
+tablaDelSiete();

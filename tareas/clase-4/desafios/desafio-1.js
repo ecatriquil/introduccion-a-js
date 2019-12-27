ac35dbe89db0e22c587904a22d943c5ff08aeee3
Emanuel Catriquil <56092756+ecatriquil@users.noreply.github.com>
2019-12-27 13:32:32 -0300

    Create desafio-1.js

---
+++ tareas/clase-4/desafios/desafio-1.js
@@ -0,0 +1,10 @@
+// Desafío de programación #1: Imprimí los números del 1 al 10
+function imprimeDiez() {
+    let i = 10;
+    while (i >= 1) {
+        console.log(i);
+        i--;
+    }
+}
+
+imprimeDiez();
